dfd2bedb2483350c963cd62238a7dfe08ff1c75a
abdulhamid4663 <abdulhamid4663@gmail.com>
2023-11-30 05:15:02 +0600

    changed baseURL in axiosSecure file

--- src/api/axiosSecure.js
+++ src/api/axiosSecure.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { clearCookie } from "./auth";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://life-flow-server.vercel.app',
     withCredentials: true
 })
 
